d1f884b6dc5405b8ee96c63fbe1c8ca91921ae01
Jan Nowak <gorskimarcin8@gmail.com>
2020-12-08 14:12:56 +0100

    contact seciotn -desktop styles, projects section, icons fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,8 @@ import '../scss/main.scss';
 
 
 console.log('HELLO 🚀')
+
+fetch("https://api.github.com/users/gorskimarcin8/repos?sort=created")
+.then((res) => res.json())
+.then((res) => {console.log(res) })
+.catch((e)=> console.log(e));
